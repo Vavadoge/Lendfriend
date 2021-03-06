aba3200367a1b043a7b1812dda0268d95b119174
vsarauskyte <sarauskyte@users.noreply.github.com>
2021-03-06 17:22:27 +0200

    work

--- app/src/main/java/com/ktu/vavadoge/Registration.java
+++ app/src/main/java/com/ktu/vavadoge/Registration.java
@@ -3,12 +3,104 @@ package com.ktu.vavadoge;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.security.ProviderInstaller;
+
+import org.json.JSONObject;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.SSLContext;
 
 public class Registration extends AppCompatActivity {
+
+    String url = "https://134.209.250.135:8080/version";
+
+    String name, username, email, password;
+    EditText input_name, input_username, input_email, input_password;
+    Button button;
+    TextView message;
+    RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        input_name = (EditText) findViewById(R.id.editTextTextPersonName);
+        input_username = (EditText) findViewById(R.id.editTextTextPersonName2);
+        input_password = (EditText) findViewById(R.id.editTextTextPassword);
+        input_email = (EditText) findViewById(R.id.editTextTextEmailAddress);
+
+        button = (Button) findViewById(R.id.button_register);
+        message = (TextView) findViewById(R.id.textView2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = input_name.getText().toString();
+                username = input_username.getText().toString();
+                password = input_password.getText().toString();
+                email = input_email.getText().toString();
+
+            }
+        });
+
+        /*try {
+            ProviderInstaller.installIfNeeded(getApplicationContext());
+        } catch (GooglePlayServicesRepairableException e) {
+            e.printStackTrace();
+        } catch (GooglePlayServicesNotAvailableException e) {
+            e.printStackTrace();
+        }*/
+
+        //try { ProviderInstaller.installIfNeeded(this); } catch (Exception e) { e.getMessage(); }
+
+        try {
+            ProviderInstaller.installIfNeeded(getApplicationContext());
+            SSLContext sslContext;
+            sslContext = SSLContext.getInstance("TLSv1.2");
+            sslContext.init(null, null, null);
+            sslContext.createSSLEngine();
+        } catch (GooglePlayServicesRepairableException | GooglePlayServicesNotAvailableException
+                | NoSuchAlgorithmException | KeyManagementException e) {
+            e.printStackTrace();
+        }
+
+        requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        message.setText("Response: " + response.toString());
+                        Log.i("tag", "test");
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        error.printStackTrace();
+                    }
+                });
+
+// Access the RequestQueue through your singleton class.
+        requestQueue.add(jsonObjectRequest);
+
     }
 }
---
+++ app/src/main/java/com/ktu/vavadoge/test_for_avatar.java
@@ -0,0 +1,14 @@
+package com.ktu.vavadoge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class test_for_avatar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_for_avatar);
+    }
+}

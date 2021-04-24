75e15d671fcc3565559ee3616fafce4335f9a43a
vsarauskyte <sarauskyte@users.noreply.github.com>
2021-04-24 19:59:20 +0300

    Bottom sheet logic

--- app/src/main/java/com/ktu/vavadoge/BottomSheetDialog.java
+++ app/src/main/java/com/ktu/vavadoge/BottomSheetDialog.java
@@ -1,16 +1,25 @@
 package com.ktu.vavadoge;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * <p>A fragment that shows a list of items as a modal bottom sheet.</p>
@@ -21,6 +30,10 @@ import android.widget.Toast;
  */
 public class BottomSheetDialog extends BottomSheetDialogFragment {
 
+    String username;
+    EditText input_username;
+    String url_add_friend = "http://134.209.250.135:8080/user/friend";
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
@@ -29,14 +42,72 @@ public class BottomSheetDialog extends BottomSheetDialogFragment {
                 container, false);
 
         Button add_friend_button = v.findViewById(R.id.add_friend_button);
+        input_username = (EditText) v.findViewById(R.id.friend_username_input);
 
         add_friend_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                username = input_username.getText().toString();
+
                 Toast.makeText(getActivity(),
-                        "Friend added", Toast.LENGTH_SHORT)
+                        username, Toast.LENGTH_SHORT)
                         .show();
                 dismiss();
+
+                JSONObject test = new JSONObject();
+                try {
+                    test.put("username", username);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                        (Request.Method.POST, url_add_friend, test, new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+
+                                Toast.makeText(getActivity(),
+                                        "yayyyyy", Toast.LENGTH_SHORT)
+                                        .show();
+                                dismiss();
+
+                            }
+                        }, new Response.ErrorListener() {
+
+                            //error message
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+
+                                try {
+                                    if(error.networkResponse != null)
+                                    {
+                                        if(error.networkResponse.data != null)
+                                        {
+                                            message.setText("Error: " + new JSONObject(new String(error.networkResponse.data)));
+                                        }
+                                        else
+                                        {
+                                            message.setText(String.valueOf(error.networkResponse.statusCode));
+                                        }
+                                    }
+                                    else
+                                    {
+                                        message.setText("null");
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        });
+
+                // Access the RequestQueue through your singleton class.
+                RequestGate.getInstance(getApplicationContext()).addToRequestQueue(jsonObjectRequest);
+
+
+
+
             }
         });
         return v;
